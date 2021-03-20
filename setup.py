a1a2632bc0c6be88fa3d26a5b461ace4a6652880
Florian Mentzel <florian.mentzel@tu-dortmund.de>
2021-03-20 16:32:35 +0100

    include setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='pybragg',
+   version='0.1',
+   description='An implementation of the [Bortfeld bragg curve fit](https://pubmed.ncbi.nlm.nih.gov/9434986/) in python with scipy and numpy.',
+   author='Florian Mentzel',
+   author_email='florian.mentzel@tu-dortmund.de',
+   install_requires=['scipy', 'numpy']
+)
