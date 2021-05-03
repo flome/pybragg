6188b855ac5e4d4c106c926ea8722c0245aa3b12
flome <florian.mentzel@tu-dortmund.de>
2021-05-03 14:11:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
    description='An implementation of the [Bortfeld bragg curve fit](https://pubmed.ncbi.nlm.nih.gov/9434986/) in python with scipy and numpy.',
    author='Florian Mentzel',
    author_email='florian.mentzel@tu-dortmund.de',
-   install_requires=['scipy', 'numpy']
+   install_requires=['scipy', 'numpy'],
+   packages=['pybragg']
 )
